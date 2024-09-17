f4ad1a66ab025fa0102e2d3f37d27b6a980fe8af
Juan <juanpanizzi@hotmail.com>
2024-09-16 23:06:12 -0300

    faq section desktop view: done

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -96,7 +96,281 @@ export const Faq = () => {
     The token is currently on the waiting list to be listed on CoinMarketCap, which is why the price will be reflected soon.
     </p>
   </details>
-  
+  <details
+    className="group rounded-lg bg-dark-light  p-6 dark:bg-gray-900 [&_summary::-webkit-details-marker]:hidden"
+  >
+    <summary
+      className="flex cursor-pointer items-center justify-between gap-1.5 text-white dark:text-white"
+    >
+      <h2 className="font-medium text-celeste lg:text-2xl">Where can I see the valuation of Atómico3?</h2>
+
+      <span className="relative size-5 shrink-0">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute inset-0 opacity-100 group-open:opacity-0"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute inset-0 opacity-0 group-open:opacity-100"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </span>
+    </summary>
+
+    <p className="mt-4 leading-relaxed text-white dark:text-gray-200 lg:text-lg">
+    You can see the current valuation of Atómico 3 on decentralized dexes like Quickswap
+    </p>
+  </details>
+  <details
+    className="group rounded-lg bg-dark-light  p-6 dark:bg-gray-900 [&_summary::-webkit-details-marker]:hidden"
+  >
+    <summary
+      className="flex cursor-pointer items-center justify-between gap-1.5 text-white dark:text-white"
+    >
+      <h2 className="font-medium text-celeste lg:text-2xl">Why does the Atómico3 token have no liquidity?</h2>
+
+      <span className="relative size-5 shrink-0">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute inset-0 opacity-100 group-open:opacity-0"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute inset-0 opacity-0 group-open:opacity-100"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </span>
+    </summary>
+
+    <p className="mt-4 leading-relaxed text-white dark:text-gray-200 lg:text-lg">
+    The token was launched on August 12 and formal liquidity will be injected when it is listed on CoinMarketCap (for which we are working on).
+    </p>
+  </details>
+  <details
+    className="group rounded-lg bg-dark-light  p-6 dark:bg-gray-900 [&_summary::-webkit-details-marker]:hidden"
+  >
+    <summary
+      className="flex cursor-pointer items-center justify-between gap-1.5 text-white dark:text-white"
+    >
+      <h2 className="font-medium text-celeste lg:text-2xl">What is the contract of Atómico3?</h2>
+
+      <span className="relative size-5 shrink-0">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute inset-0 opacity-100 group-open:opacity-0"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute inset-0 opacity-0 group-open:opacity-100"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </span>
+    </summary>
+
+    <p className="mt-4 leading-relaxed text-white dark:text-gray-200 lg:text-lg">
+    The token contract is as follows on the Polygon network: 0x22a79a08ddb74a9f1a4ebe5da75300ad9f1aed76 <br /> 
+     Projections for Atomico3: 800.000.000 Tokens At03 ||| 18 rounds: usd 1,56 ||| 34 rounds: usd 2,89 ||| 56 rounds: usd 4,6
+    </p>
+  </details>
+  <details
+    className="group rounded-lg bg-dark-light  p-6 dark:bg-gray-900 [&_summary::-webkit-details-marker]:hidden"
+  >
+    <summary
+      className="flex cursor-pointer items-center justify-between gap-1.5 text-white dark:text-white"
+    >
+      <h2 className="font-medium text-celeste lg:text-2xl">What's the price projection fot Atómico3 in the future?</h2>
+
+      <span className="relative size-5 shrink-0">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute inset-0 opacity-100 group-open:opacity-0"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute inset-0 opacity-0 group-open:opacity-100"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </span>
+    </summary>
+
+    <p className="mt-4 leading-relaxed text-white dark:text-gray-200 lg:text-lg">
+    It will behave in relation to the international lithium carbonate price (li2co3).
+    <br />
+     Link to consult the current price: <a href="https://es.investing.com/commodities/lithium-carbonate-99.5-min-china-futures?utm_source=investing_app&utm_medium=share_link&utm_campaign=share_instrument" className="text-blue-500 underline">Click here!</a> 
+      
+    </p>
+  </details>
+  <details
+    className="group rounded-lg bg-dark-light  p-6 dark:bg-gray-900 [&_summary::-webkit-details-marker]:hidden"
+  >
+    <summary
+      className="flex cursor-pointer items-center justify-between gap-1.5 text-white dark:text-white"
+    >
+      <h2 className="font-medium text-celeste lg:text-2xl">Can I now make the staking of the AT3 I have acquired?</h2>
+
+      <span className="relative size-5 shrink-0">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute inset-0 opacity-100 group-open:opacity-0"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute inset-0 opacity-0 group-open:opacity-100"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </span>
+    </summary>
+
+    <p className="mt-4 leading-relaxed text-white dark:text-gray-200 lg:text-lg">
+    Yes, I can. To do this I must click on the blue button that says “staking” located at the top of the page. Then in the following window, I will be asked to link my wallet (to be able to say from where my AT3 will come from). There I will see a form that I must fill in with my personal data and a contact email, which will allow me to choose the AT3 I want to place and the possible terms (60 days, 180 and 240). Finally, a confirmation will appear to confirm that it has been completed correctly.
+    </p>
+  </details>
+  <details
+    className="group rounded-lg bg-dark-light  p-6 dark:bg-gray-900 [&_summary::-webkit-details-marker]:hidden"
+  >
+    <summary
+      className="flex cursor-pointer items-center justify-between gap-1.5 text-white dark:text-white"
+    >
+      <h2 className="font-medium text-celeste lg:text-2xl">Why don't I see the price of Atómico3 reflected in my wallet?</h2>
+
+      <span className="relative size-5 shrink-0">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute inset-0 opacity-100 group-open:opacity-0"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute inset-0 opacity-0 group-open:opacity-100"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </span>
+    </summary>
+
+    <p className="mt-4 leading-relaxed text-white dark:text-gray-200 lg:text-lg">
+    The token is currently on the waiting list to be listed on CoinMarketCap, which is why the price will be reflected soon.
+    </p>
+  </details>
+
 </div>
     </>
   )

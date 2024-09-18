b3a8febcf8ecf6074da59f18b201be0bd062633e
Juan <juanpanizzi@hotmail.com>
2024-09-18 20:59:24 -0300

    responsive advances on VideoSection

--- src/components/Invest.tsx
+++ src/components/Invest.tsx
@@ -5,7 +5,7 @@ import support from '../assets/support.png'
 export const Invest = () => {
   return (
     <>
-    <section className="text-white mt-36 ">
+    <section className="text-white ">
   <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16 mx-auto ">
     <div className="w-full">
       <h2 className="text-3xl font-medium sm:text-7xl bg-custom-gradient bg-clip-text text-transparent w-full text-center ">Why invest in Atómico3 ?</h2>

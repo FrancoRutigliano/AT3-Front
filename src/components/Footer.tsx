ded80e39e9fe15ea4eb70538c76134ad4cc59b25
Juan <juanpanizzi@hotmail.com>
2024-09-19 09:21:19 -0300

    starting responsive styles on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,7 +17,7 @@ export const Footer = () => {
         <div className="mx-auto max-w-screen-xl space-y-8 px-4 py-16 sm:px-6 lg:space-y-16 lg:px-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <div className="text-teal-600">
-              <img src={atomicLogo} alt="" className='w-60' />
+              <img src={atomicLogo} alt="" className='w-60 mx-auto lg:mx-0' />
             </div>
 
             {/* <ul className="mt-8 flex justify-start gap-6 sm:mt-0 sm:justify-end">

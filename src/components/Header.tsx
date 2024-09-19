fc7b957d032be5dba911811a0a6318f26813d199
Juan <juanpanizzi@hotmail.com>
2024-09-19 20:00:52 -0300

    buttons hover

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,33 +28,36 @@
 //     </header>
 //   )
 // }
+import { useTranslation } from 'react-i18next'
 import atomico from '../assets/Atomico.png'
 
 export const Header = () => {
+  const [t, i18n] = useTranslation("global")
+  
   return (
     <header className='flex flex-col-reverse md:flex-row '>
       {/* Sección de texto */}
       <div className=' w-full md:w-1/2 flex items-center justify-center h-auto md:h-screen p-4  md:p-0 lg:p-0  lg:mt-10'>
         <div className=" mb-10 lg:mb-0  text-center md:text-left  md:pl-8 lg:pl-20">
-          <h1 className=' font-bold bg-custom-gradient bg-clip-text text-transparent text-5xl md:text-6xl lg:text-7xl '>Bienvenidos a Atómico3</h1>
+          <h1 className=' font-bold bg-custom-gradient bg-clip-text text-transparent text-5xl md:text-6xl lg:text-7xl '>{t("header.bienvenido")}</h1>
           {/* <p className='my-5 text-sm md:text-base lg:text-md lg:pr-20'>
             A crypto asset backed by lithium, promoting sustainability by reducing carbon emissions through tokenized incentives.
           </p> */}
           <p className='my-5 text-sm md:text-base lg:text-md lg:pr-20'>
-          Un criptoactivo respaldado por litio, que fomenta la sostenibilidad, mediante la reducción de emisiones de carbono, a través de incentivos tokenizados.
+          {t("header.criptoactivo")}
           </p>
 
 
           <div className='flex flex-col items-center md:items-start justify-between w-full lg:pr-36 space-y-3 lg:mt-3'>
-            <a href="/public/Wallpaper.pdf " download='Wallpaper.pdf' className='max-w-[270px] md:max-w-[298px] lg:max-w-[330px] border border-gray-400 text-gray-300 py-2 lg:py-3 rounded-md w-full  px-6 text-center'>
-              <button className='text-sm '>Descargar Whitepaper</button>
+            <a href="/public/Wallpaper.pdf " download='Wallpaper.pdf' className='max-w-[270px] md:max-w-[298px] lg:max-w-[330px] border border-gray-400 text-gray-300 py-2 lg:py-3 rounded-md w-full  px-6 text-center hover:bg-custom-gradient2 hover:font-bold'>
+              <button className='text-sm '>{t("header.whitepaper")}</button>
             </a>
-            <a href="#" className='max-w-[270px] md:max-w-[298px] lg:max-w-[330px] bg-custom-gradient py-2 lg:py-3 rounded-md w-full px-6 text-center'>
+            <a href="#" className='max-w-[270px] md:max-w-[298px] lg:max-w-[330px] bg-custom-gradient hover:bg-custom-gradient2 hover:font-bold py-2 lg:py-3 rounded-md w-full px-6 text-center'>
 
-              <button className='text-sm'>Comprar Token</button>
+              <button className='text-sm'>{t("header.comprar token")}</button>
             </a>
 
-            <a href="#" className='max-w-[270px] md:max-w-[298px] lg:max-w-[330px] bg-custom-gradient py-2 lg:py-3 rounded-md w-full px-6 text-center'>
+            <a href="#" className='max-w-[270px] md:max-w-[298px] lg:max-w-[330px] bg-custom-gradient hover:bg-custom-gradient2 hover:font-bold py-2 lg:py-3 rounded-md w-full px-6 text-center'>
 
               <button className='text-sm' >Staking</button>
             </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,8 @@ export default {
       },
       backgroundImage: {
         'custom-gradient': 'linear-gradient(90deg, rgba(175,33,252,1) 0%, rgba(27,190,238,1) 73%)',
+        // 'custom-gradient2': 'linear-gradient(90deg, rgba(175,33,252,1) 0%, rgba(27,190,238,1) 73%)',
+        'custom-gradient2': 'linear-gradient(90deg, rgba(177,33,253,1) 62%, rgba(98,115,245,1) 100%)'
       },
     },
 

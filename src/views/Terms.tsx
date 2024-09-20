1b2c00a2a93ab1b8826476aa43c595d07dfca6f5
Juan <juanpanizzi@hotmail.com>
2024-09-20 20:43:40 -0300

    translations

--- src/views/Terms.tsx
+++ src/views/Terms.tsx
@@ -1,9 +1,13 @@
+import { useTranslation } from "react-i18next"
 
 export const Terms = () => {
+  const [t] = useTranslation("global")
+
+
   return (
     <>
       
-      <h1 className="text-xl  font-bold lg:text-6xl lg:px-10 text-center mt-24 mb-16 lg:mt-44 lg:mb-28 bg-custom-gradient bg-clip-text text-transparent">AT3 Token Payment Terms and Conditions</h1>
+      <h1 className="text-xl  font-bold lg:text-6xl lg:px-10 text-center mt-24 mb-16 lg:mt-44 lg:mb-28 bg-custom-gradient bg-clip-text text-transparent">{t("terms.Términos y condiciones de pago del token AT3")}</h1>
       <div className="space-y-4 lg:px-10 px-5">
         <details
           className="group border-s-4 border-celeste bg-dark-light p-6 dark:bg-gray-900 [&_summary::-webkit-details-marker]:hidden "
@@ -11,7 +15,7 @@ export const Terms = () => {
         >
           <summary className="flex cursor-pointer items-center justify-between gap-1.5">
             <h2 className="text-sm lg:text-2xl font-medium text-celeste">
-              General
+            {t("terms.General")}
             </h2>
 
             <span
@@ -32,46 +36,47 @@ export const Terms = () => {
             </span>
           </summary>
 
-          <p className="text-[0.8rem] mt-4 leading-relaxed text-gray-300 ">
-            These AT3 Token Payment Terms and Conditions ("AT3 Token Terms") apply to the use of the AT3 token as your selected payment method for eligible Atómico3.io products and services in certain jurisdictions. Please read these AT3 Token Terms carefully. By using AT3 Token as your payment method, you acknowledge and agree to be bound by these AT3 Token Terms. These AT3 Token Terms apply in addition to the terms and conditions contained in the Order Agreement and the Product Purchase Agreement. In the event of a conflict between these AT3 Token Terms and the Order Agreement or Product Purchase Agreement regarding transactions with the AT3 Token, these AT3 Token Terms will apply. If you do not agree with any of the terms and conditions in these AT3 Token Terms, please do not select AT3 Token as your payment method. We reserve the right to change, suspend, remove, disable or impose restrictions or limits on our acceptance of the AT3 token as a payment method for our products and services at any time and without liability to you, in our sole discretion.
+          <p className="text-[1rem} mt-4 leading-relaxed text-gray-300 ">
+          {t("terms.General descripcion")}
           </p>
         </details>
 
-        <details
-          className="group border-s-4 border-celeste bg-dark-light p-6 dark:bg-gray-900 [&_summary::-webkit-details-marker]:hidden"
-        >
-          <summary className="flex cursor-pointer items-center justify-between gap-1.5">
-            <h2 className="text-sm lg:text-2xl font-medium text-celeste">
-              AT3 Token as a Payment Method
-            </h2>
+          <details
+            className="group border-s-4 border-celeste bg-dark-light p-6 dark:bg-gray-900 [&_summary::-webkit-details-marker]:hidden"
+          >
+            <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+              <h2 className="text-sm lg:text-2xl font-medium text-celeste">
+              {t("terms.El token AT3 como método de pago")}
+              </h2>
 
-            <span
-              className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3 dark:bg-gray-800 dark:text-white"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="size-5 shrink-0 transition duration-300 group-open:-rotate-45"
-                viewBox="0 0 20 20"
-                fill="currentColor"
+              <span
+                className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3 dark:bg-gray-800 dark:text-white"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </span>
-          </summary>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="size-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+            </summary>
 
-          <p className="text-[0.8rem] mt-4 leading-relaxed text-gray-300">
-            When prompted during the purchase process, we may, at our discretion, allow you to select AT3 Token as a payment method for the purchase of eligible products and services in certain jurisdictions, as detailed in our AT3 Token Payments FAQ at www.atomic3.io/support/token-at3, which we may update from time to time. We may require you to provide us with additional information to verify your identity when selecting the AT3 token as your payment method. To make a payment in AT3 token, you need a digital wallet that supports AT3 token transfers to external digital wallets, compatible hardware, and internet access. <br />
-
-            If you decide to make an eligible purchase with AT3 token, you must select AT3 token as your payment method when prompted. We will provide you with a digital wallet address ("AT3 Token Address") in alphanumeric form and in QR code. This is the payment location where you will need to send the AT3 token from your digital wallet. You can copy and paste or manually enter the alphanumeric code we provide you into the recipient field in your digital wallet software, or if your digital wallet software supports QR codes, scan the QR code we provide using your device to auto-fill the recipient field with the alphanumeric code.
+            <p className="text-[1rem} mt-4 leading-relaxed text-gray-300">
+            {t("terms.El token AT3 como método de pago p1")}<br />
             <br />
-            <br />
-            <span className="font-bold text-celeste">IMPORTANT NOTE:</span> It is your responsibility to confirm and verify that the alphanumeric code provided is identical to the alphanumeric code displayed in your digital wallet software by scanning the QR code or copying and pasting the alphanumeric code. We will not be responsible for any loss of AT3 token or other digital assets as a result of sending AT3 token to an incorrect wallet address or sending a digital asset other than AT3 token.
-          </p>
-        </details>
+            {t("terms.El token AT3 como método de pago p2")}
+            
+              <br />
+              <br />
+              <span className="font-bold text-celeste">{t("terms.nota importante")}:</span> {t("terms.nota importante descripcion")}
+            </p>
+          </details>
 
 
 
@@ -80,7 +85,7 @@ export const Terms = () => {
         >
           <summary className="flex cursor-pointer items-center justify-between gap-1.5">
             <h2 className="text-sm lg:text-2xl font-medium text-celeste">
-              AT3 Token Network Commissions
+             {t("terms.Comisiones de la red del token AT3")}
             </h2>
 
             <span
@@ -101,8 +106,9 @@ export const Terms = () => {
             </span>
           </summary>
 
-          <p className="text-[0.8rem] mt-4 leading-relaxed text-gray-300">
-            You are responsible for paying directly all network fees associated with the transaction (“Transaction Fees”), in addition to the AT3 Token Price. If your digital wallet software does not automatically generate an amount for Transaction Fees, you will need to calculate the amount necessary for the transaction to be processed on the AT3 token network. For more details on Transaction Fees, please see the AT3 Token FAQ at www.atomic3.io/support/token-at3. The Transaction Fee covers the cost of verifying AT3 token transactions. We have no control over the Transaction Fee and do not receive any part of it.
+          <p className="text-[1rem} mt-4 leading-relaxed text-gray-300">
+            {t("terms.Comisiones de la red del token AT3 descripcion")}
+        
           </p>
         </details>
 
@@ -111,7 +117,7 @@ export const Terms = () => {
         >
           <summary className="flex cursor-pointer items-center justify-between gap-1.5">
             <h2 className="text-sm lg:text-2xl font-medium text-celeste">
-              AT3 Token Payment Support
+             {t("terms.Soporte de pago de tokens AT3")}
             </h2>
 
             <span
@@ -132,10 +138,12 @@ export const Terms = () => {
             </span>
           </summary>
 
-          <p className="text-[0.8rem] mt-4 leading-relaxed text-gray-300">
-            The AT3 token is the only digital asset we accept as payment for our products at this time. You cannot make purchases with us using any other digital assets, including other cryptocurrency tokens. Our digital wallet for AT3 token is not configured to detect or receive digital assets other than the AT3 token. <br />
+          <p className="text-[1rem} mt-4 leading-relaxed text-gray-300">
+          {t("terms.Soporte de pago de tokens AT3 descripcion p1")}
+          <br />
             <span className="text-celeste">
-              MAKE SURE YOU DO NOT SEND DIGITAL ASSETS OTHER THAN THE AT3 TOKEN. IF YOU ATTEMPT TO SEND A DIGITAL ASSET TYPE OTHER THAN THE AT3 TOKEN, THAT DIGITAL ASSET MAY BE LOST OR IRRETRIEVABLY DESTROYED. WE WILL NOT BE RESPONSIBLE FOR ANY LOST OR DESTROYED DIGITAL ASSETS.
+            {t("terms.Soporte de pago de tokens AT3 descripcion p2")}
+
             </span>
           </p>
         </details>
@@ -145,7 +153,7 @@ export const Terms = () => {
         >
           <summary className="flex cursor-pointer items-center justify-between gap-1.5">
             <h2 className="text-sm lg:text-2xl font-medium text-celeste">
-              Transactions Completed once Confirmed
+             {t("terms.Transacciones completadas una vez confirmadas")}
             </h2>
 
             <span
@@ -166,8 +174,9 @@ export const Terms = () => {
             </span>
           </summary>
 
-          <p className="text-[0.8rem] mt-4 leading-relaxed text-gray-300">
-            Once an AT3 token transaction is sent to the network, it will be unconfirmed for a period of time (generally less than an hour, but up to a day or more) until the network reflects such transfer in a specified number of consecutive blocks in the AT3 token network. Such number will be determined by us at our discretion. We will consider your transaction pending until the AT3 token network reflects the transaction in the required number of consecutive blocks. We will send you an email communication once we have received your payment.
+          <p className="text-[1rem} mt-4 leading-relaxed text-gray-300">
+          {t("terms.Transacciones completadas una vez confirmadas descripcion")}
+
           </p>
         </details>
 
@@ -176,7 +185,7 @@ export const Terms = () => {
         >
           <summary className="flex cursor-pointer items-center justify-between gap-1.5">
             <h2 className="text-sm lg:text-2xl font-medium text-celeste">
-              Underpayments and Overpayments
+             {t("terms.Pagos insuficientes y excesivos")}
             </h2>
 
             <span
@@ -197,12 +206,14 @@ export const Terms = () => {
             </span>
           </summary>
 
-          <p className="text-[0.8rem] mt-4 text-gray-300">
-            The price of the AT3 token can be volatile and subject to up and down movements. You are responsible for initiating an AT3 token payment for the exact amount indicated on the payment page within the AT3 Token Price Window. Each AT3 Token Price will expire after the AT3 Token Price Window. After the AT3 Token Price expires, you will need to request a new AT3 Token Price and AT3 Token Price Window on the payment page and you may need to restart the payment process. <br />
-            Regarding order fees and/or deposits, if you transmit an amount other than the Price in AT3 Token, your order may fail, we may refund your order fee and/or deposit under these AT3 Token Terms, and you must resubmit payment in the correct amount in order for us to process your order. <br />
-            <span className="text-celeste"> Regarding final payments: </span> <br />
-            <span className="text-celeste">  (i)</span> if you transfer less than the AT3 Token Price, we will retain your payment and apply it to the amount due, and you will be required to pay the outstanding balance. <br />
-            <span className="text-celeste"> (ii) </span> if you transfer more than the AT3 Token Price, you will not be entitled to a refund of the excess amount.
+          <p className="text-[1rem} mt-4 text-gray-300">
+          {t("terms.Pagos insuficientes y excesivos p1")}
+          <br />
+          {t("terms.Pagos insuficientes y excesivos p2")}
+          <br />
+            <span className="text-celeste"> {t("terms.En cuanto a los pagos finales")}: </span> <br />
+            <span className="text-celeste">  (i)</span> {t("terms.i")} <br />
+            <span className="text-celeste"> (ii) </span>  {t("terms.ii")}
           </p>
         </details>
 
@@ -232,7 +243,7 @@ export const Terms = () => {
             </span>
           </summary>
 
-          <p className="text-[0.8rem] mt-4 text-gray-300">
+          <p className="text-[1rem} mt-4 text-gray-300">
             Once you initiate an AT3 token transaction, the transaction cannot be reversed. This is inherent in the nature of the AT3 token network and is not a policy set by us.  <br />
             <span className="text-celeste"> YOU ARE RESPONSIBLE FOR VERIFYING THAT YOU HAVE ENTERED THE CORRECT AT3 TOKEN ADDRESS IN THE RECIPIENT FIELD AND THE CORRECT AT3 TOKEN PRICE IN THE AMOUNT FIELD IN YOUR DIGITAL WALLET SOFTWARE.</span>
           </p>
@@ -264,7 +275,7 @@ export const Terms = () => {
             </span>
           </summary>
 
-          <p className="text-[0.8rem] mt-4 text-gray-300">
+          <p className="text-[1rem} mt-4 text-gray-300">
           We do not own or control the underlying software protocols that govern the operation of the AT3 token network. The AT3 token network protocols are open source and may be used, copied, modified and distributed by anyone. We are not responsible for the operation of the AT3 token network protocols. The AT3 token network protocols are subject to sudden changes in operating rules (e.g., forks), and such changes may materially affect the value, function and/or even the name of the AT3 token.
           </p>
         </details>

67eeb55b26bed655f7e9da36bab32899ba843bdd
Juan <juanpanizzi@hotmail.com>
2024-09-23 11:12:31 -0300

    scroll to top solution solved

--- src/routes/RoutesOutlet.tsx
+++ src/routes/RoutesOutlet.tsx
@@ -3,10 +3,12 @@ import { MainLayout } from "../views/MainLayout"
 import { Home } from "../views/Home"
 import { Terms } from "../views/Terms"
 import { StakingView } from "../views/StakingView"
+import ScrollToTop from "../components/ScrollToTop"
 
 export const RoutesOutlet = () => {
     return (
         <HashRouter>
+              <ScrollToTop />
             <Routes>
                 <Route path="/" element={<MainLayout/>}>
 

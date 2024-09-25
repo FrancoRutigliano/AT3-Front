384849620a49a555b460d1a2472fd639bccf1e84
Juan <juanpanizzi@hotmail.com>
2024-09-24 22:52:23 -0300

    advances on new

--- src/views/Press.tsx
+++ src/views/Press.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import litio from '../assets/news/Litio.jpeg'
+import at3 from '../assets/news/at3-image.jpeg'
+
 export const Press = () => {
   return (
     <section className=" text-white">
-  <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-    <div className="text-center w-full">
-      <h2 className="text-3xl font-bold sm:text-4xl">Noticias</h2>
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+        <div className="text-center w-full">
+          <h2 className="text-3xl font-bold sm:text-4xl">Noticias</h2>
 
-      <p className="mt-4 text-gray-300 ">
-      Explora las tendencias más recientes en el mundo del litio, el mineral que impulsa la innovación tecnológica. Conoce cómo su creciente demanda está transformando las industrias globales y descubre cómo Atómico3, nuestra criptomoneda, está revolucionando el mercado al estar vinculada a este recurso clave. ¡No te pierdas las noticias que podrían definir el futuro de la energía y la economía digital!
-      </p>
-    </div>
+          <p className="mt-4 text-gray-300 ">
+            Explora las tendencias más recientes en el mundo del litio, el mineral que impulsa la innovación tecnológica. Conoce cómo su creciente demanda está transformando las industrias globales y descubre cómo Atómico3, nuestra criptomoneda, está revolucionando el mercado al estar vinculada a este recurso clave. ¡No te pierdas las noticias que podrían definir el futuro de la energía y la economía digital!
+          </p>
+        </div>
 
-    <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-      <a
-        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-celeste hover:shadow-celeste text-center"
-        href="https://nacionweb.com.ar/aumento-del-precio-del-litio-impulsa-a-la-criptomoneda-argentina-atomico-3-como-opcion-de-inversion-solida/"
-        target='_blank'
-      >
-        {/* <svg
+        <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+          <a
+            className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-celeste hover:shadow-celeste text-center"
+            href="https://nacionweb.com.ar/aumento-del-precio-del-litio-impulsa-a-la-criptomoneda-argentina-atomico-3-como-opcion-de-inversion-solida/"
+            target='_blank'
+          >
+            {/* <svg
           xmlns="http://www.w3.org/2000/svg"
           className="size-10 text-pink-500"
           fill="none"
@@ -36,182 +38,170 @@ export const Press = () => {
             d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
           />
         </svg> */}
-        <img src={litio} alt="at3" className='text-left w-full  max-h-[200px] object-cover rounded-lg' />
+            <img src={at3} alt="at3" className='text-left w-full h-[200px] object-cover rounded-lg' />
 
-        <h2 className="mt-4 text-xl font-bold text-white text-left ml-2">Aumento del precio del litio impulsa a la criptomoneda Argentina Atómico3 como opción de inversión sólida</h2>
+            <h2 className="mt-4 text-xl font-bold text-white text-left ml-2">
+              Aumento del precio del litio impulsa a la criptomoneda Argentina Atómico3 como opción de inversión sólida
+            </h2>
 
-        {/* <p className="mt-1 text-sm text-gray-300">
+            {/* <p className="mt-1 text-sm text-gray-300">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
           distinctio alias voluptatum blanditiis laudantium.
         </p> */}
 
-      </a>
+          </a>
 
-      <a
-        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-celeste hover:shadow-celeste"
-        href="#"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="size-10 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-          <path
-            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-          />
-        </svg>
+          <a
+            className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-celeste hover:shadow-celeste"
+            target='_blank'
+            href="https://agenciapolitica.com.ar/la-valorizacion-del-litio-en-latinoamerica-perspectivas-proyecciones-y-su-impacto-en-el-mercado-global/"
+          >
+            <img src={litio} alt="litio" className='text-left w-full  max-h-[200px] object-cover rounded-lg' />
 
-        <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
 
+
+            <h2 className="mt-4 text-xl font-bold text-white ml-2">La valorización del litio en Latinoamérica: perspectivas, proyecciones y su impacto en el mercado global</h2>
+            {/* 
         <p className="mt-1 text-sm text-gray-300">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
           distinctio alias voluptatum blanditiis laudantium.
-        </p>
-      </a>
+        </p> */}
+          </a>
 
-      <a
-        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-celeste hover:shadow-celeste"
-        href="#"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="size-10 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-          <path
-            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-          />
-        </svg>
+          <a
+            className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-celeste hover:shadow-celeste"
+            href="#"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="size-10 text-pink-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path d="M12 14l9-5-9-5-9 5 9 5z" />
+              <path
+                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+              />
+            </svg>
 
-        <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
+            <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
 
-        <p className="mt-1 text-sm text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
-          distinctio alias voluptatum blanditiis laudantium.
-        </p>
-      </a>
+            <p className="mt-1 text-sm text-gray-300">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
+              distinctio alias voluptatum blanditiis laudantium.
+            </p>
+          </a>
 
-      <a
-        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-celeste hover:shadow-celeste"
-        href="#"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="size-10 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-          <path
-            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-          />
-        </svg>
+          <a
+            className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-celeste hover:shadow-celeste"
+            href="#"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="size-10 text-pink-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path d="M12 14l9-5-9-5-9 5 9 5z" />
+              <path
+                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+              />
+            </svg>
 
-        <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
+            <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
 
-        <p className="mt-1 text-sm text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
-          distinctio alias voluptatum blanditiis laudantium.
-        </p>
-      </a>
+            <p className="mt-1 text-sm text-gray-300">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
+              distinctio alias voluptatum blanditiis laudantium.
+            </p>
+          </a>
 
-      <a
-        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-celeste hover:shadow-celeste"
-        href="#"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="size-10 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-          <path
-            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-          />
-        </svg>
+          <a
+            className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-celeste hover:shadow-celeste"
+            href="#"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="size-10 text-pink-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path d="M12 14l9-5-9-5-9 5 9 5z" />
+              <path
+                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+              />
+            </svg>
 
-        <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
+            <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
 
-        <p className="mt-1 text-sm text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
-          distinctio alias voluptatum blanditiis laudantium.
-        </p>
-      </a>
+            <p className="mt-1 text-sm text-gray-300">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
+              distinctio alias voluptatum blanditiis laudantium.
+            </p>
+          </a>
 
-      <a
-        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-celeste hover:shadow-celeste"
-        href="#"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="size-10 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-          <path
-            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-          />
-        </svg>
+          <a
+            className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-celeste hover:shadow-celeste"
+            href="#"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="size-10 text-pink-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path d="M12 14l9-5-9-5-9 5 9 5z" />
+              <path
+                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+              />
+            </svg>
 
-        <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
+            <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
 
-        <p className="mt-1 text-sm text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
-          distinctio alias voluptatum blanditiis laudantium.
-        </p>
-      </a>
-    </div>
+            <p className="mt-1 text-sm text-gray-300">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
+              distinctio alias voluptatum blanditiis laudantium.
+            </p>
+          </a>
+        </div>
 
-    <div className="mt-12 text-center">
-      <a
-        href="#"
-        className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400"
-      >
-        Get Started Today
-      </a>
-    </div>
-  </div>
-</section>
+        <div className="mt-12 text-center">
+          <a
+            href="#"
+            className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400"
+          >
+            Get Started Today
+          </a>
+        </div>
+      </div>
+    </section>
   )
 }

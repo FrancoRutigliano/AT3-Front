55187906a4644115e6266a3f128565a79b8ed81f
Juan <juanpanizzi@hotmail.com>
2024-09-25 20:12:51 -0300

    mision

--- src/components/Valores.tsx
+++ src/components/Valores.tsx
@@ -1,4 +1,7 @@
 
+import manos from '../assets/valores/manos.png'
+import mision from '../assets/valores/mision.png'
+import objetivo from '../assets/valores/objetivo.png'
 
 export const Valores = () => {
   return (
@@ -18,30 +21,12 @@ export const Valores = () => {
       <a
         className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-purple-black hover:shadow-purple-black"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="size-10 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-          <path
-            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-          />
-        </svg>
+        <img src={manos} alt="valores" />
 
-        <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
+        <h2 className="mt-4 text-xl font-bold text-white">Valores</h2>
 
         <p className="mt-1 text-sm text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
-          distinctio alias voluptatum blanditiis laudantium.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, veritatis voluptatibus. Dolore similique optio voluptatem delectus a aliquam tenetur, doloremque velit cupiditate placeat culpa. Laborum quia reprehenderit illo tempore voluptatibus.
         </p>
       </a>
 
@@ -49,30 +34,13 @@ export const Valores = () => {
         className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-purple-black hover:shadow-purple-black"
         href="#"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="size-10 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-          <path
-            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-          />
-        </svg>
+       <img src={mision} alt="valores" />
 
-        <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
+        <h2 className="mt-4 text-xl font-bold text-white">Visión</h2>
 
         <p className="mt-1 text-sm text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
-          distinctio alias voluptatum blanditiis laudantium.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas error fuga velit? Laborum earum aliquid natus temporibus eligendi expedita debitis assumenda incidunt beatae nemo, adipisci, iusto accusamus odio. Quae, magnam!
+
         </p>
       </a>
 
@@ -80,30 +48,12 @@ export const Valores = () => {
         className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-purple-black hover:shadow-purple-black"
         href="#"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="size-10 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-          <path
-            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-          />
-        </svg>
+        <img src={objetivo} alt="" />
 
-        <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
+        <h2 className="mt-4 text-xl font-bold text-white">Misión</h2>
 
         <p className="mt-1 text-sm text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
-          distinctio alias voluptatum blanditiis laudantium.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ad, eaque magnam totam molestiae fugit. Blanditiis voluptas dicta distinctio debitis placeat vitae, incidunt dolorum odit iste libero tempora officia? Doloribus.
         </p>
       </a>
 

4e9b621134cde3cc42be3628d925ce255aaabe00
Juan <juanpanizzi@hotmail.com>
2024-09-25 19:01:06 -0300

    First part of about us

--- src/views/Nosotros.tsx
+++ src/views/Nosotros.tsx
@@ -5,35 +5,32 @@ export const Nosotros = () => {
   return (
     <>
 
-      <section
-        className="overflow-hidden bg-cover bg-no-repeat bg-red-200"
-        style={{
-          backgroundImage: `url(${at3})`,
-          backgroundPosition: 'center 50%', // Ajusta el porcentaje para que la imagen se vea más abajo
-          backgroundColor: 'red'
-        }}
-      >
-        <div className="p-8 md:p-12 lg:px-16 lg:py-24">
-          <div className="text-center ltr:sm:text-left rtl:sm:text-right flex flex-col justify-center items-center mb-10">
-            {/* <h2 className="text-2xl font-bold text-white sm:text-3xl md:text-5xl text-left">Atómico3</h2> */}
-            <img src={atomico3} alt="atomico" className='w-1/3 max-w-[550px] ' />
-            <p className="hidden w-2/3 text-white/90 md:mt-6 md:block md:text-lg md:leading-relaxed text-center">
-              Somos una compañía legalmente constituida en Paraguay, dedicada al desarrollo y optimización de actividades mineras con un enfoque
-              integral en la cadena de valor de la electromovilidad en Latinoamérica.
+<section
+  className=""
+  
+>
+  <div className="overflow-hidden  h-1/3 md:h-1/2 lg:h-screen bg-cover bg-no-repeat p-8 md:p-12 lg:px-16 lg:py-24" style={{
+    background: `linear-gradient(to bottom, rgba(0, 0, 18, 0.7), rgba(0, 0, 36, 0.7)), url(${at3})`,
+    backgroundPosition: 'center 50%', 
+    backgroundRepeat: 'no-repeat'
+}}>
+    <div className="text-center ltr:sm:text-left rtl:sm:text-right flex flex-col justify-center items-center mb-10">
+      <img src={atomico3} alt="atomico" className='w-2/3 lg:w-1/3 max-w-[550px]' />
+      <p className=" w-full lg:w-2/3   mt-2  md:text-lg text-center text-xs ">
+        Somos una compañía legalmente constituida en Paraguay, dedicada al desarrollo y optimización de actividades mineras con un enfoque
+        integral en la cadena de valor de la electromovilidad en Latinoamérica.
+      </p>
+      <div className="mt-4 sm:mt-8 flex justify-start">
+        {/* Tu botón u otros elementos aquí */}
+      </div>
+    </div>
+  </div>
+</section>
 
-            </p>
+<section className='h-screen' style={{ backgroundColor: '#000024' }}>
+  <h1 className='mt-20'>Hola como estas</h1>
+</section>
 
-            <div className="mt-4 sm:mt-8 flex justify-start">
-              {/* <a
-          href="#"
-          className="inline-block rounded-full bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
-        >
-       Conócenos
-        </a> */}
-            </div>
-          </div>
-        </div>
-      </section>
     </>
   )
 }

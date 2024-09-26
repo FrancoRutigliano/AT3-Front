92d503a53199fca1b79841fd9a65db722a14efda
Juan <juanpanizzi@hotmail.com>
2024-09-26 08:39:10 -0300

    Nosotros view added

--- src/components/Valores.tsx
+++ src/components/Valores.tsx
@@ -6,161 +6,67 @@ import objetivo from '../assets/valores/objetivo.png'
 export const Valores = () => {
   return (
     <>
-     <section className=" text-white">
-  <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-    <div className="mx-auto max-w-lg text-center">
-      <h2 className="text-3xl font-bold sm:text-4xl">Nuestros Valores</h2>
+      <section className=" text-white">
+        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+          <div className="mx-auto  text-center">
+            <h2 className="text-3xl font-bold sm:text-6xl mt-10 lg:mb-24">Nuestros Valores</h2>
 
-      {/* <p className="mt-4 text-gray-300">
+            {/* <p className="mt-4 text-gray-300">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur aliquam doloribus
         nesciunt eos fugiat. Vitae aperiam fugit consequuntur saepe laborum.
       </p> */}
-    </div>
+          </div>
 
-    <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-      <a
-        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-purple-black hover:shadow-purple-black"
-      >
-        <img src={manos} alt="valores" />
+          <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+            <a
+              className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-purple-black hover:shadow-purple-black"
+            >
+              <img src={manos} alt="valores" />
 
-        <h2 className="mt-4 text-xl font-bold text-white">Valores</h2>
+              <h2 className="mt-4 text-xl font-bold text-white">Propósito</h2>
 
-        <p className="mt-1 text-sm text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, veritatis voluptatibus. Dolore similique optio voluptatem delectus a aliquam tenetur, doloremque velit cupiditate placeat culpa. Laborum quia reprehenderit illo tempore voluptatibus.
-        </p>
-      </a>
+              <p className="mt-1 text-sm text-gray-300">
+                Compromiso con el crecimiento sostenible y el desarrollo económico global. Trabajar para crear un futuro donde los recursos se gestionen de manera responsable y con visión de largo plazo, en beneficio tanto de los inversores como de las comunidades.
+              </p>
+            </a>
 
-      <a
-        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-purple-black hover:shadow-purple-black"
-        href="#"
-      >
-       <img src={mision} alt="valores" />
+            <a
+              className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-purple-black hover:shadow-purple-black"
+              href="#"
+            >
+              <img src={mision} alt="valores" />
 
-        <h2 className="mt-4 text-xl font-bold text-white">Visión</h2>
+              <h2 className="mt-4 text-xl font-bold text-white">Visión</h2>
 
-        <p className="mt-1 text-sm text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas error fuga velit? Laborum earum aliquid natus temporibus eligendi expedita debitis assumenda incidunt beatae nemo, adipisci, iusto accusamus odio. Quae, magnam!
+              <p className="mt-1 text-sm text-gray-300">
+                Queremos revolucionar la manera en que se gestionan y se invierte
+                en recursos naturales mediante la aplicación de tecnologías avanzadas de
+                tokenización y blockchain.
+              </p>
+            </a>
 
-        </p>
-      </a>
+            <a
+              className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-purple-black hover:shadow-purple-black"
+              href="#"
+            >
+              <img src={objetivo} alt="" />
 
-      <a
-        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-purple-black hover:shadow-purple-black"
-        href="#"
-      >
-        <img src={objetivo} alt="" />
+              <h2 className="mt-4 text-xl font-bold text-white">Misión</h2>
 
-        <h2 className="mt-4 text-xl font-bold text-white">Misión</h2>
+              <p className="mt-1 text-sm text-gray-300">
+                Mejorar la eficiencia y la transparencia del mercado de
+                litio, abrir nuevas oportunidades para inversores y
+                contribuir al desarrollo económico global.
+              </p>
+            </a>
 
-        <p className="mt-1 text-sm text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ad, eaque magnam totam molestiae fugit. Blanditiis voluptas dicta distinctio debitis placeat vitae, incidunt dolorum odit iste libero tempora officia? Doloribus.
-        </p>
-      </a>
 
-      <a
-        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-purple-black hover:shadow-purple-black"
-        href="#"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="size-10 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-          <path
-            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-          />
-        </svg>
 
-        <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
 
-        <p className="mt-1 text-sm text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
-          distinctio alias voluptatum blanditiis laudantium.
-        </p>
-      </a>
+          </div>
 
-      <a
-        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-purple-black hover:shadow-purple-black"
-        href="#"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="size-10 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-          <path
-            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-          />
-        </svg>
-
-        <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
-
-        <p className="mt-1 text-sm text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
-          distinctio alias voluptatum blanditiis laudantium.
-        </p>
-      </a>
-
-      <a
-        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-purple-black hover:shadow-purple-black"
-        href="#"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="size-10 text-pink-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path d="M12 14l9-5-9-5-9 5 9 5z" />
-          <path
-            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-          />
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-          />
-        </svg>
-
-        <h2 className="mt-4 text-xl font-bold text-white">Digital campaigns</h2>
-
-        <p className="mt-1 text-sm text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut quo possimus adipisci
-          distinctio alias voluptatum blanditiis laudantium.
-        </p>
-      </a>
-    </div>
-
-    <div className="mt-12 text-center">
-      <a
-        href="#"
-        className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400"
-      >
-        Get Started Today
-      </a>
-    </div>
-  </div>
-</section>   
+        </div>
+      </section>
     </>
   )
 }
--- src/views/Nosotros.tsx
+++ src/views/Nosotros.tsx
@@ -6,31 +6,33 @@ export const Nosotros = () => {
   return (
     <>
 
-<section
-  className=""
-  
->
-  <div className="overflow-hidden  h-1/3 md:h-1/2 lg:h-screen bg-cover bg-no-repeat p-8 md:p-12 lg:px-16 lg:py-24" style={{
-    background: `linear-gradient(to bottom, rgba(0, 0, 18, 0.7), rgba(0, 0, 36, 0.7)), url(${at3})`,
-    backgroundPosition: 'center 50%', 
-    backgroundRepeat: 'no-repeat'
-}}>
-    <div className="text-center ltr:sm:text-left rtl:sm:text-right flex flex-col justify-center items-center mb-10">
-      <img src={atomico3} alt="atomico" className='w-2/3 lg:w-1/3 max-w-[550px]' />
-      <p className=" w-full lg:w-2/3   mt-2  md:text-lg text-center text-xs ">
-        Somos una compañía legalmente constituida en Paraguay, dedicada al desarrollo y optimización de actividades mineras con un enfoque
-        integral en la cadena de valor de la electromovilidad en Latinoamérica.
-      </p>
-      <div className="mt-4 sm:mt-8 flex justify-start">
-        {/* Tu botón u otros elementos aquí */}
-      </div>
-    </div>
-  </div>
-</section>
+      <section
+        className=""
 
-<section className='h-screen' style={{ backgroundColor: '#000024' }}>
-  <Valores/>
-</section>
+      >
+        <div className="overflow-hidden  h-1/3 md:h-1/2 lg:h-screen bg-cover bg-no-repeat p-8 md:p-12 lg:px-16 lg:py-24" style={{
+          background: `linear-gradient(to bottom, rgba(0, 0, 18, 0.7), rgba(0, 0, 36, 0.7)), url(${at3})`,
+          backgroundPosition: 'center 50%',
+          backgroundRepeat: 'no-repeat'
+        }}>
+          <div className="text-center ltr:sm:text-left rtl:sm:text-right flex flex-col justify-center items-center mb-10">
+            <img src={atomico3} alt="atomico" className='w-2/3 lg:w-1/3 max-w-[550px]' />
+            <p className=" w-full lg:w-2/3   mt-2  md:text-lg text-center text-xs ">
+              Somos una compañía legalmente constituida en Paraguay, dedicada al desarrollo y optimización de actividades mineras con un enfoque orientado a una herramienta de tokenización para las empresas mineras y relacionadas en toda la cadena de electromovilidad.
+
+              {/* integral en la cadena de valor de la electromovilidad en Latinoamérica.
+              orientada a una herramienta de tokenización para las empresas mineras y relacionadas en toda la cadena de electromovilidad. */}
+            </p>
+            <div className="mt-4 sm:mt-8 flex justify-start">
+              {/* Tu botón u otros elementos aquí */}
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='h-screen' style={{ backgroundColor: '#000024' }}>
+        <Valores />
+      </section>
 
     </>
   )

78bea0c3f3d3b8bfe30da786219c2dbf4db306ff
Juan <juanpanizzi@hotmail.com>
2024-09-26 17:30:43 -0300

    traductions on about us done

--- src/views/Nosotros.tsx
+++ src/views/Nosotros.tsx
@@ -10,7 +10,7 @@ export const Nosotros = () => {
     <>
 
       <section
-        className="bg-gradient-to-b from-[#000017] to-transparent"
+        className=""
 
       >
         {/* <div className="overflow-hidden  h-1/3 md:h-1/2 lg:h-screen bg-cover bg-no-repeat p-8 md:p-12 lg:px-16 lg:py-24" style={{
@@ -23,12 +23,15 @@ export const Nosotros = () => {
     className="absolute inset-0 bg-cover bg-center z-0"
     style={{ backgroundImage: `url('${at3}')` }}
   />
-  <div className="absolute inset-0 bg-black/50   opacity-70 z-10" /> {/* Overlay más oscuro */}
+  <div className="absolute inset-0 bg-black/50  opacity-100 z-10" /> {/* Overlay más oscuro */}
   
   <div className="relative z-20 flex flex-col justify-center items-center  h-full lg:mb-20">
     <img src={atomico3} alt="atomico" className='w-2/3 lg:w-1/3 max-w-[550px]' />
     <p className="w-full lg:w-2/3 mt-2 md:text-lg text-center text-xs">
       {t("nosotros.title")}
+      <br />
+      {t("nosotros.title p1")}
+
     </p>
     <div className="mt-4 sm:mt-8 flex justify-start">
       {/* Tu botón u otros elementos aquí */}
@@ -40,7 +43,7 @@ export const Nosotros = () => {
         {/* </div> */}
       </section>
 
-      <section className='inset-x-0 top-0 h-30 bg-gradient-to-b from-[#000017] to-transparent' style={{ backgroundColor: '#000024' }}>
+      <section className='inset-x-0 top-0 h-30 bg-gradient-to-b from-[#000012] to-transparent' style={{ backgroundColor: '#000024' }}>
         <Valores />
       </section>
 

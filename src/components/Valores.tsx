6b551d9b0e555512b3b5c69efff15a6d47ad10b3
Juan <juanpanizzi@hotmail.com>
2024-09-26 09:37:43 -0300

    valores responsive done

--- src/components/Valores.tsx
+++ src/components/Valores.tsx
@@ -1,15 +1,18 @@
 
+import { useTranslation } from 'react-i18next'
 import manos from '../assets/valores/manos.png'
 import mision from '../assets/valores/mision.png'
 import objetivo from '../assets/valores/objetivo.png'
 
 export const Valores = () => {
+  const [t] = useTranslation("global")
+
   return (
     <>
       <section className=" text-white">
         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
           <div className="mx-auto  text-center">
-            <h2 className="text-3xl font-bold sm:text-6xl mt-10 lg:mb-24">Nuestros Valores</h2>
+            <h2 className="text-3xl font-bold sm:text-6xl lg:mt-10 lg:mb-24">{t("nosotros.Nuestros Valores")}</h2>
 
             {/* <p className="mt-4 text-gray-300">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur aliquam doloribus
@@ -21,42 +24,40 @@ export const Valores = () => {
             <a
               className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-purple-black hover:shadow-purple-black"
             >
-              <img src={manos} alt="valores" />
+              <img className="max-w-[35px] md:max-w-[50px] lg:max-w-[50px]" src={manos} alt="valores" />
 
-              <h2 className="mt-4 text-xl font-bold text-white">Propósito</h2>
+              <h2 className="mt-4 text-xl font-bold text-white">{t("nosotros.Propósito")}</h2>
 
               <p className="mt-1 text-sm text-gray-300">
-                Compromiso con el crecimiento sostenible y el desarrollo económico global. Trabajar para crear un futuro donde los recursos se gestionen de manera responsable y con visión de largo plazo, en beneficio tanto de los inversores como de las comunidades.
+              {t("nosotros.Propósito descripcion")}
               </p>
             </a>
 
             <a
               className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-purple-black hover:shadow-purple-black"
-              href="#"
             >
-              <img src={mision} alt="valores" />
+              <img className="max-w-[35px] md:max-w-[50px] lg:max-w-[50px]" src={mision} alt="valores" />
 
-              <h2 className="mt-4 text-xl font-bold text-white">Visión</h2>
+              <h2 className="mt-4 text-xl font-bold text-white">{t("nosotros.Visión")}</h2>
 
               <p className="mt-1 text-sm text-gray-300">
-                Queremos revolucionar la manera en que se gestionan y se invierte
-                en recursos naturales mediante la aplicación de tecnologías avanzadas de
-                tokenización y blockchain.
+              {t("nosotros.Visión descripcion")}
+                
               </p>
             </a>
 
             <a
               className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-purple-black hover:shadow-purple-black"
-              href="#"
             >
-              <img src={objetivo} alt="" />
+              <img className="max-w-[35px] md:max-w-[50px] lg:max-w-[50px]" src={objetivo} alt="" />
 
-              <h2 className="mt-4 text-xl font-bold text-white">Misión</h2>
+              <h2 className="mt-4 text-xl font-bold text-white">{t("nosotros.Misión")}
+              </h2>
 
               <p className="mt-1 text-sm text-gray-300">
-                Mejorar la eficiencia y la transparencia del mercado de
-                litio, abrir nuevas oportunidades para inversores y
-                contribuir al desarrollo económico global.
+              {t("nosotros.Misión descripcion")}
+
+              
               </p>
             </a>
 
--- src/views/Nosotros.tsx
+++ src/views/Nosotros.tsx
@@ -2,7 +2,10 @@ import at3 from '../assets/lineas.png'
 // import at3 from '../assets/Atomico.png'
 import atomico3 from '../assets/atomicLogo.png'
 import { Valores } from '../components/Valores'
+import { useTranslation } from 'react-i18next'
 export const Nosotros = () => {
+  const [t] = useTranslation("global")
+
   return (
     <>
 
@@ -18,8 +21,7 @@ export const Nosotros = () => {
           <div className="text-center ltr:sm:text-left rtl:sm:text-right flex flex-col justify-center items-center mb-10">
             <img src={atomico3} alt="atomico" className='w-2/3 lg:w-1/3 max-w-[550px]' />
             <p className=" w-full lg:w-2/3   mt-2  md:text-lg text-center text-xs ">
-              Somos una compañía legalmente constituida en Paraguay, dedicada al desarrollo y optimización de actividades mineras con un enfoque orientado a una herramienta de tokenización para las empresas mineras y relacionadas en toda la cadena de electromovilidad.
-
+              {t("nosotros.title")}
               {/* integral en la cadena de valor de la electromovilidad en Latinoamérica.
               orientada a una herramienta de tokenización para las empresas mineras y relacionadas en toda la cadena de electromovilidad. */}
             </p>
@@ -30,7 +32,7 @@ export const Nosotros = () => {
         </div>
       </section>
 
-      <section className='h-screen' style={{ backgroundColor: '#000024' }}>
+      <section className='' style={{ backgroundColor: '#000024' }}>
         <Valores />
       </section>
 

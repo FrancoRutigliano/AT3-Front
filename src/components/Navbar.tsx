2f848669f6c1a87b02f64daf3714ebddc5fc37ab
Juan <juanpanizzi@hotmail.com>
2024-10-02 12:47:23 -0300

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,7 @@ export const Navbar = () => {
   const [t, i18n] = useTranslation("global")
   const [isOpen, setIsOpen] = useState(false); // Estado para controlar si el menú está abierto o cerrado
   const [isOpenDropdown, setIsOpenDropdown] = useState(false); // Estado para controlar si el menú está abierto o cerrado
+  const [isOpenDropdownHelp, setIsOpenDropdownHelp] = useState(false); // Estado para controlar si el menú está abierto o cerrado
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -25,6 +26,9 @@ export const Navbar = () => {
 
   const toggleDropdown = () => {
     setIsOpenDropdown(prev => !prev);
+
+  }; const toggleDropdownHelp = () => {
+    setIsOpenDropdownHelp(prev => !prev);
   };
 
   return (
@@ -130,22 +134,6 @@ export const Navbar = () => {
                         >
                           {t("navbar.tutoriales")}
                         </Link>
-                        {/* <a
-        href="#"
-        className="block rounded-lg px-4 py-2 text-sm text-white hover:bg-gray-50 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-300"
-        role="menuitem"
-      >
-        Como hacer P2P
-      </a> */}
-
-                        {/* <a
-        href="#"
-        className="block rounded-lg px-4 py-2 text-sm text-white hover:bg-gray-50 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-300"
-        role="menuitem"
-      >
-        Done estamos tokenizando
-      </a> */}
-
 
                       </div>
                     </div>
@@ -178,31 +166,69 @@ export const Navbar = () => {
                     {t("navbar.noticias")}
                   </Link>
                 </li>
-                <li>
-                  <a
-                    className="text-white transition hover:text-celeste dark:text-white hover:cursor-pointer"
-                    // href="#contact"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      const element = document.getElementById('contact');
-                      if (element) { // Verificamos si el elemento no es null
-                        element.scrollIntoView({ behavior: 'smooth' });
-                      }
-                      closeMenu();
-                    }}
+
+                {/* DROPDWON AYUDA*/}
+                <div className="relative"
+                  onMouseLeave={() => setIsOpenDropdownHelp(false)}>
+                  <div
+                    className="inline-flex items-center overflow-hidden rounded-md  text-white"
                   >
 
-                    {t("navbar.contacto")}
-                  </a>
-                </li>
-                {/* <li>
-                  <Link
-                    className="text-white transition hover:text-celeste dark:text-white "
-                    to={"/soporte"}
-                  >
-                    {t("navbar.soporte")}
-                  </Link> 
-                </li> */}
+                    <button
+                      className="h-full flex items-center p-2 text-white  dark:text-gray-300 hover:text-blue-500 "
+                      onMouseEnter={() => setIsOpenDropdownHelp(true)}
+
+                    >
+                      <a
+                        className=" mr-1  py-2 text-sm/none text-white hover:text-blue-500 "
+                      >
+                        {t("navbar.ayuda")}
+                      </a>
+                      <span className="sr-only">Menu</span>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="size-4 "
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+
+                  {
+                    isOpenDropdownHelp &&
+                    <div
+                      className="absolute end-0 z-10 mt-0 w-56 bg-dark-light text-white shadow-lg dark:border-gray-800 dark:bg-gray-900"
+                      role="menu"
+                    >
+                      <div className="p-2">
+                        <a
+                          href='https://dapp-at-3.vercel.app'
+                          className="block rounded-lg px-4 py-2 text-sm text-white hover:bg-gray-50 hover:text-blue-500"
+                          role="menuitem"
+                          target='_blank'
+                        >
+                          Contacto
+                        </a>
+                        <Link
+                          to={'/soporte'}
+                          className="block rounded-lg px-4 py-2 text-sm text-white hover:bg-gray-50 hover:text-blue-500"
+                          role="menuitem"
+                        >
+                          Soporte
+                        </Link>
+                       
+                      </div>
+                    </div>
+                  }
+                </div>
+                {/* FIN DROPDWON AYUDA*/}
+
                 <li>
                   <button onClick={() => handleChangeLanguage("en")}>
                     <img src={eng} className='w-[1rem] lg:w-[2rem] ml-2 transition-shadow duration-300 hover:shadow-lg hover:shadow-blue-500  hover:scale-105  rounded-full' alt="english" />
@@ -346,23 +372,6 @@ export const Navbar = () => {
                         {t("navbar.tutoriales")}
                       </Link>
 
-                      {/* <a
-        href="#"
-        className="block rounded-lg px-4 py-2 text-sm text-white hover:bg-gray-50 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-300"
-        role="menuitem"
-      >
-        Como hacer P2P
-      </a> */}
-
-                      {/* <a
-        href="#"
-        className="block rounded-lg px-4 py-2 text-sm text-white hover:bg-gray-50 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-800 dark:hover:text-gray-300"
-        role="menuitem"
-      >
-        Done estamos tokenizando
-      </a> */}
-
-
                     </div>
                   </div>
                 }
@@ -408,7 +417,7 @@ export const Navbar = () => {
                     closeMenu();
                   }}
                 >
-                  {t("navbar.contacto")}
+                  {t("navbar.ayuda")}
                 </a>
               </li>
               <li>

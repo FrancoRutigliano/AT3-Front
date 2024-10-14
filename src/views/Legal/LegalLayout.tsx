8527d9fcd8b8dafafaface5eddaead6f751c7336
Juan <juanpanizzi@hotmail.com>
2024-10-14 11:33:58 -0300

    advances

--- src/views/Legal/LegalLayout.tsx
+++ src/views/Legal/LegalLayout.tsx
@@ -1,22 +1,30 @@
-import { Link, Outlet } from "react-router-dom"
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 export const LegalLayout = () => {
+  const location = useLocation(); // Obtiene la ubicación actual
+
   return (
     <>
- <nav className="mt-20">
-    <ul className="flex justify-center ">
-    <li>
-        <Link to={'terms'}>Terms and conditions</Link>
-    </li>
-    <li>
-        <Link to={'privacy'}>Privacy Policy</Link>
-    </li>
-    <li>
-        <Link to={'cookies'}>Cookies policy</Link>
-    </li>
- </ul>
- </nav>
-        <Outlet/>
+      <nav className="mt-20">
+        <ul className="flex justify-center w-2/3 mx-auto space-x-4">
+          <li className={`lg:text-2xl ${location.pathname === '/legal/terms' ? 'border-b-2 border-blue-500' : 'border-b-2 border-transparent'}`}>
+            <Link to={'terms'} className="text-white">
+              Terms and conditions
+            </Link>
+          </li>
+          <li className={`lg:text-2xl ${location.pathname === '/legal/privacy' ? 'border-b-2 border-blue-500' : 'border-b-2 border-transparent'}`}>
+            <Link to={'privacy'} className="text-white">
+              Privacy Policy
+            </Link>
+          </li>
+          <li className={`lg:text-2xl ${location.pathname === '/legal/cookies' ? 'border-b-2 border-blue-500' : 'border-b-2 border-transparent'}`}>
+            <Link to={'cookies'} className="text-white">
+              Cookies policy
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
     </>
-  )
-}
+  );
+};

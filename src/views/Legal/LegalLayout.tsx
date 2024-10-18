f0e81a76443287ec789769754323ff918fdcc7ba
Juan <juanpanizzi@hotmail.com>
2024-10-18 08:01:12 -0300

    adv on navbar

--- src/views/Legal/LegalLayout.tsx
+++ src/views/Legal/LegalLayout.tsx
@@ -7,21 +7,21 @@ export const LegalLayout = () => {
     <>
       <nav className="mt-20">
         <ul className="flex justify-center w-2/3  mx-auto space-x-6">
-          <li className={`p-5 lg:text-2xl ${location.pathname === '/legal/privacy' ? 'border-b-4 border-blue-500' : 'border-b-4 border-transparent'}`}>
-            <Link to={'privacy'} className="text-white">
+          <li className={`p-5 lg:text-2xl ${location.pathname === '/legal/privacy' ? 'border-b-4 border-blue-500 transition duration-150 ease-out hover:ease-in text-center' : ' text-center border-b-4 border-transparent'}`}>
+            <Link to={'privacy'} className=" text-white hover:text-blue-500 transition duration-150 ease-out hover:ease-in">
               Privacy Policy
             </Link>
           </li>
-          <li className={`p-5 lg:text-2xl ${location.pathname === '/legal/terms' ? 'border-b-4 border-blue-500' : 'border-b-4 border-transparent'}`}>
-            <Link to={'terms'} className="text-white">
+          <li className={`p-5 lg:text-2xl ${location.pathname === '/legal/terms' ? 'border-b-4 border-blue-500 transition duration-150 ease-out hover:ease-in text-center ' : ' text-center border-b-4 border-transparent'}`}>
+            <Link to={'terms'} className=" text-white hover:text-blue-500 transition duration-150 ease-out hover:ease-in">
               Terms and conditions
             </Link>
           </li>
-          <li className={`p-5 lg:text-2xl ${location.pathname === '/legal/cookies' ? 'border-b-4 border-blue-500' : 'border-b-2 border-transparent'}`}>
-            <Link to={'cookies'} className="text-white">
+          {/* <li className={`p-5 lg:text-2xl ${location.pathname === '/legal/cookies' ? 'border-b-4 border-blue-500 transition duration-150 ease-out hover:ease-in text-center ' : 'text-center  border-b-2 border-transparent'}`}>
+            <Link to={'cookies'} className=" text-white hover:text-blue-500 transition duration-150 ease-out hover:ease-in">
               Cookies policy
             </Link>
-          </li>
+          </li> */}
         </ul>
       </nav>
       <Outlet />

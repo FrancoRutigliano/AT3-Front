6c642dceac149dbcf875e4306b555e7d753d8ca6
Juan <125908889+JuanPanizzi@users.noreply.github.com>
2024-10-22 10:15:29 -0300

    advances

--- src/components/Staking.tsx
+++ src/components/Staking.tsx
@@ -42,15 +42,15 @@ export const Staking = () => {
             <img
               alt="staking"
               src={i18n.language === 'es' ? stakingES : stakingEN}
-              className='max-w-[200px] lg:max-w-[400px]  w-2/3 mx-auto object-contain lg:object-contain rounded-md'
+              className='hidden md:block max-w-[200px] lg:max-w-[400px]  w-2/3 mx-auto object-contain lg:object-contain rounded-md'
             />
           </div>
-          <div className="mx-auto col-span-2  md:p-0 md:w-full px-5  text-center lg:text-left ltr:sm:text-left rtl:sm:text-right">
+          <div className="mx-auto sm:col-span-2   md:p-0 md:w-full px-10 md:px-5  text-center lg:text-left ltr:sm:text-left rtl:sm:text-right">
             <h2 className="mb-5  w-full bg-custom-gradient bg-clip-text text-transparent text-xl font-bold lg:text-6xl py-2 ">
               {t("stakingView.¿Qué es el Staking?")}
             </h2>
 
-            <p className=" text-sm text-white md:mt-4 md:block   lg:text-base break-words">
+            <p className=" text-xs text-white md:mt-4 md:block text-justify md:text-center  md:px-0 lg:text-base break-words">
               {/* {t("stakingView.¿Qué es el Staking? Descripcion")} */}
               {t("stakingView.staking p1")}
                <br />
@@ -61,7 +61,7 @@ export const Staking = () => {
             </p>
             {/* <p className=" text-sm text-white  md:block  lg:text-lg break-words" >{t("stakingView.¿Qué es el Staking? Descripcion 2")}</p> */}
 
-            <div className="mt-4 md:mt-8 flex justify-center lg:justify-start ">
+            <div className="mt-4 md:mt-8 flex justify-between md:justify-center  lg:justify-start ">
               <Link to={'/ComoInvertir#tutoriales'} className='w-2/3 max-w-[150px]  lg:max-w-[200px] md:w-full bg-custom-gradient2 hover:bg-custom-gradient   py-2 lg:py-2 rounded-md px-6 text-xs sm:text-sm lg:text-base text-center hover:cursor-pointer'  >
                {t("invertir.ver tutoriales")}
               </Link>
@@ -79,7 +79,7 @@ export const Staking = () => {
               src={staking2}
 
               // className="rounded-lg w-2/3 object-cover sm:h-96 my-auto "
-              className='max-w-[200px] lg:max-w-[400px]  w-2/3 mx-auto object-contain lg:object-contain rounded-md'
+              className='hidden md:block max-w-[200px] lg:max-w-[400px]  w-2/3 mx-auto object-contain lg:object-contain rounded-md'
             />
           </div>
         </section>

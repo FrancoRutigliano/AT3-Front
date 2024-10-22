91f34110e5b3722dfcfcea2d8cb69f304c8d0ae5
Juan <125908889+JuanPanizzi@users.noreply.github.com>
2024-10-22 11:34:50 -0300

    peer to peer responsive

--- src/components/PeerToPeer.tsx
+++ src/components/PeerToPeer.tsx
@@ -35,30 +35,31 @@ export const PeerToPeer = () => {
     }, [location]);
     return (
         <>
-            <section className='mt-16' ref={p2pRef} id='p2p'>
+            <section className='mt-16 ' ref={p2pRef} id='p2p'>
                 {/* <h1 className='bg-custom-gradient bg-clip-text text-transparent text-4xl lg:text-7xl text-center font-bold lg:mt-32  pt-5' >Peer to Peer</h1> */}
-                <section className=" lg:mt-0 flex flex-col-reverse overflow-hidden  sm:grid sm:grid-cols-2 py-10">
-                    <div className="py-5 px-1 md:p-12 lg:pl-7 lg:pr-2  lg:py-24 ">
-                        <div className="mx-auto p-5 md:p-0 md:w-full  text-center lg:text-left ltr:sm:text-left rtl:sm:text-right">
+                <section className=" lg:mt-0 flex flex-col-reverse overflow-hidden  sm:grid sm:grid-cols-3 py-10">
+                    <div className=" sm:col-span-2 py-5  md:p-12 lg:pl-7 lg:pr-2  lg:py-24 ">
+                        <div className=" mx-auto px-10  md:p-0 md:w-full  text-center lg:text-left ltr:sm:text-left rtl:sm:text-right">
                             <h2 className="mb-5 bg-custom-gradient bg-clip-text text-transparent text-2xl lg:text-6xl font-bold md:text-3xl py-2 break-words">
                                 {t("como invertir.peer to peer")}
 
                             </h2>
 
-                            <p className='text-sm text-white md:mt-4 md:block  lg:text-lg break-words'>
-                                {/* {t("como invertir.peer to peer p1")} */}
-                                Los pagos P2P o peer-to-peer (literalmente, de persona a persona), son transferencias instantáneas entre particulares que se realizan a través de una plataforma digital, en este caso, se envían entre billeteras digitales, por un lado, los token de AT3 contra los USDT o USDC. <br />
-<br />
-                                De esta forma podes comprar tus atomicos directamente de nosotros desde la billetera oficial 0XAFF655C15C943121DEA79B67C47AC9BD2253FD65, pagandolos con tus USDT o USDC
-
+                            <p className=' md:pr-12 text-justify  text-xs ms:text-sm text-white md:mt-4 md:block  lg:text-base break-words'>
+                                {t("como invertir.peer to peer p1")}
+                               <br />
+                                <br />
+                                {t("como invertir.peer to peer p2")}
+                                <br />
+                                <br />
+                                {t("como invertir.peer to peer p2.1")}
+                                
 
                             </p>
-                            {/* <p className='text-sm text-white md:mt-4 md:block  lg:text-lg break-words'>
-                                {t("como invertir.peer to peer p2")}
-                            </p> */}
 
 
-                            <div className="mt-4 md:mt-8 flex justify-center lg:justify-start ">
+
+                            <div className="mt-4 md:mt-8 flex justify-between md:justify-center lg:justify-start ">
                                 <Link to={'/ComoInvertir#tutoriales'} className='w-2/3 max-w-[150px] lg:max-w-[200px] md:w-full bg-custom-gradient2 hover:bg-custom-gradient   py-2 lg:py-2 rounded-md px-6 text-xs sm:text-sm lg:text-lg text-center hover:cursor-pointer'  >
 
                                     <button className=''>{t("invertir.ver tutoriales")}</button>
@@ -70,70 +71,21 @@ export const PeerToPeer = () => {
                             </div>
                         </div>
 
-
-
-
-
-
-
-
                     </div>
-                    <div className=' flex justify-center items-center px-5 md:px-0' >
+                    <div className='flex justify-center items-center px-5 md:px-0' >
 
                         <img
-                            alt=""
-                            // src={i18n.language === 'es' ? stakingES : stakingEN}
+                            alt="Peer to peer"
                             src={p2p}
 
-                            className='rounded-lg  lg:object-contain max-w-[400px]  lg:max-w-[350px]'
+                            className='rounded-lg max-w-[150px] lg:object-contain   lg:max-w-[350px]'
 
                         />
-                        {/* <img
-                            alt=""
-                            // src={i18n.language === 'es' ? stakingES : stakingEN}
-                            src={p23}
 
-                            className='rounded-lg  lg:object-contain max-w-[400px]  lg:max-w-[600px]'
-
-                        /> */}
                     </div>
                 </section>
             </section>
-            {/* TUTORIAL */}
-            {/* <section className='mt-28'>
-                <section className="">
-                    <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-                        <div className="max-w-3xl">
-                            <h2 className="text-center lg:text-left bg-custom-gradient bg-clip-text text-transparent text-3xl md:text-3xl lg:text-6xl font-bold">
-                                {t("como invertir.peer to peer p3")}
 
-                            </h2>
-                        </div>
-
-                        <div className="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
-
-                            <div className="relative overflow-hidden pb-[56.25%] h-0 max-w-[600px] w-full mx-auto">
-                                <iframe className="absolute top-0 left-0 w-full h-full rounded-xl" width="560" height="315" src="https://www.youtube.com/embed/nnGQIAwvpBs?si=RFUnBQZ1RBBmT1N6" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                            </div>
-                            <div className="lg:py-16">
-                                <article className="space-y-4 text-gray-200">
-                                    <p className="text-md lg:text-2xl text-center lg:text-left">
-                                        {t("como invertir.peer to peer p4")}
-
-                                    </p>
-
-                                    <p className="text-md lg:text-2xl text-center lg:text-left">
-                                        {t("como invertir.peer to peer p5")}
-
-                                    </p>
-                                </article>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </section> */}
         </>
     )
 }
-
-{/* <iframe width="560" height="315" src="https://www.youtube.com/embed/nnGQIAwvpBs?si=0z8VyvJh38nMeiNZ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe> */ }

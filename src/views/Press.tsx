7501094145eabacd55ed87928256de61407a0eb5
Juan <125908889+JuanPanizzi@users.noreply.github.com>
2024-10-23 17:06:43 -0300

    advances

--- src/views/Press.tsx
+++ src/views/Press.tsx
@@ -1,6 +1,8 @@
 import litio from '../assets/news/Litio.jpeg'
 import at3 from '../assets/news/at3-image.jpeg'
 import at3img from '../assets/news/at3-image2.jpeg'
+import n4 from '../assets/news/n4.webp'
+
 import { useTranslation } from 'react-i18next'
 import { Noticia } from '../components/Noticia'
 export const Press = () => {
@@ -10,6 +12,8 @@ export const Press = () => {
     {imagen: at3, title:  t("noticias.n1"), subtitle: "Nación Web",link: "https://nacionweb.com.ar/aumento-del-precio-del-litio-impulsa-a-la-criptomoneda-argentina-atomico-3-como-opcion-de-inversion-solida/" },
     {imagen: litio, title:  t("noticias.n2"), subtitle: "Agencia Política" ,link: "https://agenciapolitica.com.ar/la-valorizacion-del-litio-en-latinoamerica-perspectivas-proyecciones-y-su-impacto-en-el-mercado-global/" },
     {imagen: at3img, title:  t("noticias.n3"), subtitle: "Agencia Política" ,link: "https://noticiasargentinas.com/economia/atomico-3--la-criptomoneda-argentina-ligada-al-litio-reduce-su-emision-para-que-sea-sostenible-en-el-tiempo_a669c39acd066f26ecefe0568?fbclid=IwZXh0bgNhZW0CMTAAAR2t5kt4DbjXVmaGOKH-lOZuFmAcLYq2iKPRhkVG9j5xc9LjCc5X0gpPaIg_aem_b1TPAFvZgRThufzAXn8i3Q" },
+    {imagen: n4, title:  t("noticias.n4"), subtitle: "El Economista" ,link: "https://eleconomista.com.ar/energia/cual-mineral-podria-alcanzar-us-8700-millones-exportaciones-2030-n78652#google_vignette" },
+
   ]
 
   return (

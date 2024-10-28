8aa7e498f32ec37a2eae08e481dac40177d1e335
Juan <125908889+JuanPanizzi@users.noreply.github.com>
2024-10-28 10:42:48 -0300

    punto 8

--- src/views/Legal/FaqForLaw.tsx
+++ src/views/Legal/FaqForLaw.tsx
@@ -108,15 +108,26 @@ export const FaqForLaw = () => {
 
                     </section> <section id="section3">
 
-                        <h2 className="text-lg md:text-3xl font-semibold mt-6 mb-2 text-blue-500">7. {t("faqForLaw.titles.title3")}</h2>
+                        <h2 className="text-lg md:text-3xl font-semibold mt-6 mb-2 text-blue-500">7. {t("faqForLaw.titles.title7")}</h2>
 
-                        <p className="mt-5 mb-10">{t("faqForLaw.documento.informacion punto 3.p1")}</p>
+                        <p className="mt-5 ">{t("faqForLaw.documento.informacion punto 7.p1")} <span className="text-blue-500 underline">
+                             support@atomico3.io</span> {t("faqForLaw.documento.informacion punto 7.p1.1")}</p>
+                        <p className="mt-5 mb-10">{t("faqForLaw.documento.informacion punto 7.p2")}</p>
+
 
                     </section> <section id="section5">
 
-                        <h2 className="text-lg md:text-3xl font-semibold mt-6 mb-2 text-blue-500">{t("faqForLaw.titles.title3")}</h2>
+                        <h2 className="text-lg md:text-3xl font-semibold mt-6 mb-2 text-blue-500">8. {t("faqForLaw.titles.title8")}</h2>
 
-                        <p className="mt-5 mb-10">{t("faqForLaw.documento.informacion punto 3.p1")}</p>
+                        {[
+                            { text: t("faqForLaw.documento.informacion punto 8.p1") },
+                            { text: t("faqForLaw.documento.informacion punto 8.p2") },
+                            { text: t("faqForLaw.documento.informacion punto 8.p3") }
+                        ].map((item, index) => (
+                            <>
+                                <p className="mb-4" key={`item-${index}`} >{item.text}</p>
+                            </>
+                        ))}
 
                     </section>
 

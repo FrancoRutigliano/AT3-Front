dbaef0ddaafe150c9376f1498a4f7ec9b53b3b2f
Juan <juanpanizzi@hotmail.com>
2024-11-12 10:34:02 -0300

    avances responsive

--- src/components/ProyectosTokenizacion/LitioInfo.tsx
+++ src/components/ProyectosTokenizacion/LitioInfo.tsx
@@ -18,7 +18,7 @@ export const LitioInfo = () => {
       // Ajustar el scroll para que se desplace 100px más arriba
       window.scrollTo({
         top: elementPosition - 100, // Ajusta la posición sumando o restando píxeles
-        behavior: 'smooth', 
+        behavior: 'smooth',
       });
     }
   }, [location]);
@@ -48,19 +48,74 @@ export const LitioInfo = () => {
         </div>
         <div className="flex flex-col">
 
-        <h2 className="lg:hidden py-5 text-2xl font-bold text-blue-500 md:text-4xl text-center lg:text-left">
-              {t("proyectosTokenizacion.que es el litio")}
-            </h2>
-        <img
-          alt="litio"
-          src={litio}
-          className=" w-4/5 md:px-0 rounded-xl md:h-full md:w-3/4 object-cover sm:h-[calc(100%_-_2rem)] sm:self-end  md:h-[calc(100%_-_4rem)]  
+          <h2 className="lg:hidden py-5 text-2xl font-bold text-blue-500 md:text-4xl text-center lg:text-left">
+            {t("proyectosTokenizacion.que es el litio")}
+          </h2>
+          <img
+            alt="litio"
+            src={litio}
+            className=" w-4/5 md:px-0 rounded-xl md:h-full md:w-3/4 object-cover sm:h-[calc(100%_-_2rem)] sm:self-end  md:h-[calc(100%_-_4rem)]  
           max-[1023px]:max-w-[600px] mx-auto md:max-w-[500px]"
           />
-          </div>
+        </div>
       </section>
-      <section>
-        <section className="bg-gray-50 text-black px-10">
+
+<div>
+
+      <div className="grid grid-cols-2 bg-gray-50">
+        <div className=" col-span-1 ">
+      <h2 className="  mx-auto max-w-2xl    hidden lg:block text-2xl font-bold text-blue-500 bg-gray-50 md:text-4xl text-center md:text-left lg:px-16 xl:px-6">
+            {t("proyectosTokenizacion.por que es tan importante el litio")}
+            </h2>
+        </div>
+</div>
+      </div>
+      <section ref={litioRef} className=" flex flex-col-reverse overflow-hidden bg-gray-50 lg:grid lg:grid-cols-2 lg:items-center ">
+
+        <div className=" p-8 lg:px-16 ">
+            
+          <div className=" mx-auto max-w-2xl text-center ltr:sm:text-left rtl:sm:text-right">
+
+            <p className=" text-dark-light md:mt-4 md:block text-justify">
+              {t("proyectosTokenizacion.p5")}
+
+            </p>
+            <p className=" text-dark-light md:mt-4 md:block text-justify">
+              {t("proyectosTokenizacion.p6")}
+
+              <br /> <br />
+              {t("proyectosTokenizacion.p7")}
+
+            </p>
+            {/* <p className="text-gray-500 text-lg text-start my-4 underline deco">Leer más</p> */}
+          </div>
+        </div>
+        <div className="flex flex-col">
+
+          <h2 className="lg:hidden py-5 text-2xl font-bold text-blue-500 md:text-4xl text-center lg:text-left">
+            {t("proyectosTokenizacion.por que es tan importante el litio")}
+          </h2>
+          <img
+            alt="litio salar"
+            src={salar}
+            className=" w-4/5 md:px-0 rounded-xl  md:w-3/4 object-cover 
+            sm:h-[calc(100%_-_2rem)]
+            md:h-[calc(100%_-_4rem)]
+            max-[1023px]:max-w-[600px] 
+            
+            mx-auto 
+            md:max-w-[600px]
+            h-full
+              sm:self-end 
+              lg:h-full lg:w-full
+              "
+          />
+        </div>
+      </section>
+
+
+
+      {/* <section className="bg-gray-50 text-black px-10">
           <div className="mx-auto  px-1 py-16 sm:px-1 sm:py-10 lg:px-1   ">
             <div className="max-w-3xl ">
               <h2 className="text-2xl font-bold sm:text-4xl text-center md:text-left text-blue-500 ">
@@ -88,16 +143,20 @@ export const LitioInfo = () => {
                   </p>
 
                   <p className="text-justify text-dark-light">
-                  {t("proyectosTokenizacion.p7")}
+                    {t("proyectosTokenizacion.p7")}
 
                   </p>
                 </article>
               </div>
             </div>
           </div>
-        </section>
+        </section> */}
 
-      </section>
+
+
+
+
+
       {/* <section className="lg:py-32 bg-white">
         <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
           <div className="mx-auto max-w-3xl text-center">
